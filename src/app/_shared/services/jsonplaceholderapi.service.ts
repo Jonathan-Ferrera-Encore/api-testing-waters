62b09c7467129653b5304905c2fd65b22f19610a
Jonathan Brian Ferrera <Jonathan.ferrera@psav.com>
2024-11-12 16:40:47 -0600

    JSONPlaceholder Start Formatting
    Starting the format process to build upon as more functions are incorporated.

--- src/app/_shared/services/jsonplaceholderapi.service.ts
+++ src/app/_shared/services/jsonplaceholderapi.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, timer } from 'rxjs';
-import { take, takeUntil } from 'rxjs/operators';
+import { retry, take, takeUntil } from 'rxjs/operators';
 
 import { JsonPlaceholderPost } from '../interfaces/jsonplaceholder';
 
@@ -14,16 +14,16 @@ export class JsonPlaceholderApiService {
 
     getPosts(): Observable<JsonPlaceholderPost[]> {
         const destruct: Observable<0> = timer(5000);
-        return this.http.get<JsonPlaceholderPost[]>(this.baseUrl + '/posts').pipe(takeUntil(destruct), take(1));
+        return this.http.get<JsonPlaceholderPost[]>(this.baseUrl + '/posts').pipe(takeUntil(destruct), take(1), retry(1));
     }
 
     getPostsById(id: number): Observable<JsonPlaceholderPost> {
         const destruct: Observable<0> = timer(5000);
-        return this.http.get<JsonPlaceholderPost>(this.baseUrl + '/posts/' + id).pipe(takeUntil(destruct), take(1));
+        return this.http.get<JsonPlaceholderPost>(this.baseUrl + '/posts/' + id).pipe(takeUntil(destruct), take(1), retry(1));
     }
 
     postPosts(jsonPlaceholderPost: JsonPlaceholderPost): Observable<JsonPlaceholderPost> {
         const destruct: Observable<0> = timer(5000);
-        return this.http.post<JsonPlaceholderPost>(this.baseUrl + '/posts', jsonPlaceholderPost).pipe(takeUntil(destruct), take(1));
+        return this.http.post<JsonPlaceholderPost>(this.baseUrl + '/posts', jsonPlaceholderPost).pipe(takeUntil(destruct), take(1), retry(1));
     }
 }
--- src/app/jsonplaceholder/jsonplaceholder.component.ts
+++ src/app/jsonplaceholder/jsonplaceholder.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import {MatTabsModule} from '@angular/material/tabs';
+
 @Component({
   selector: 'app-jsonplaceholder',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './jsonplaceholder.component.html',
   styleUrl: './jsonplaceholder.component.scss'
 })
