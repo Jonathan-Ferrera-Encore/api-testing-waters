60c403e2b03c679fdf038b83b333421629a1fd8f
Jonathan Brian Ferrera <Jonathan.ferrera@psav.com>
2024-11-21 11:17:40 -0600

    Initialize Angular Material Theming
    Initial styles. Also, testing if verified commits are the default.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'waters-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'waters-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'api-testing-waters';
+    
 }
