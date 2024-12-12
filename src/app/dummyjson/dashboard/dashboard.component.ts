2ea37120bf25de18d759cc895e91ef40ab348ca8
Jonathan Brian Ferrera <Jonathan.ferrera@psav.com>
2024-12-12 11:42:59 -0600

    Dashboard Update
    Update the Dashboard component for prepare for links to other API routes.

--- src/app/dummyjson/dashboard/dashboard.component.ts
+++ src/app/dummyjson/dashboard/dashboard.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'waters-dashboard',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {
 
