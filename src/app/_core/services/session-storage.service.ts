2f56319b3c3d346c5373cc679e98e7df1d449e65
Jonathan Brian Ferrera <Jonathan.ferrera@psav.com>
2025-06-11 15:13:53 -0500

    Upgrade to Angular 20
    From 19.1.7 to 20.0.3.

--- src/app/_core/services/session-storage.service.ts
+++ src/app/_core/services/session-storage.service.ts
@@ -1,5 +1,5 @@
-import { Inject, Injectable } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable, DOCUMENT } from '@angular/core';
+
 
 import { IStorage } from '../interfaces/storage';
 import { ConsoleService } from './console.service';
