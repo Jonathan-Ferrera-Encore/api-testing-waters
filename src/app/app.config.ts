8848a22885c912bb11f513d01af20a68ebb77b84
Jonathan Brian Ferrera <Jonathan.ferrera@psav.com>
2025-08-05 16:31:11 -0500

    Convert Event Replay to Incremental Hydration
    Event Replay is enabled by Incremental Hydration, so both settings are not needed.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration, withIncrementalHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
         provideBrowserGlobalErrorListeners(),
         provideZonelessChangeDetection(),
         provideRouter(routes),
-        provideClientHydration(withEventReplay()),
+        provideClientHydration(withIncrementalHydration()),
         provideAnimationsAsync(),
         provideHttpClient(withFetch())
     ]
